cd37e9b8aca2d78fa2f0109c664ab22e0f558435
Degurechaff97 <lunaplaysdota@gmail.com>
2024-07-23 15:20:12 +0300

    Update template.php

--- template.php
+++ template.php
@@ -6,50 +6,33 @@
 /** @global CDatabase $DB */
 /** @var CBitrixComponentTemplate $this */
 /** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
+/**  @var string $templateFile */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
+
+// Подключаем CSS файл
+$this->addExternalCss($templateFolder."/css/common.css");
 ?>
-<div class="news-list">
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
-<?endif;?>
-<div class="news-column">
-<?
-$itemCount = count($arResult["ITEMS"]);
-$halfCount = ceil($itemCount / 2);
-$counter = 0;
-?>
+
+<div class="article-list">
 <?foreach($arResult["ITEMS"] as $arItem):?>
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?>
-	<div class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
-			<h2 class="news-title">
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><?echo $arItem["NAME"]?></a>
-			<?else:?>
-				<?echo $arItem["NAME"]?>
-			<?endif;?>  
-			</h2>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<p class="news-description"><?echo $arItem["PREVIEW_TEXT"];?></p>
-		<?endif;?>
-	</div>
-	<?
-	$counter++;
-	if ($counter == $halfCount):
-	?>
-		</div><div class="news-column">
-	<?endif;?>
+    <?
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
+    <a class="article-item article-list__item" href="<?=$arItem["DETAIL_PAGE_URL"]?>" data-anim="anim-3" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+        <div class="article-item__background">
+            <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+                <img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>" />
+            <?endif;?>
+        </div>
+        <div class="article-item__wrapper">
+            <div class="article-item__title"><?=$arItem["NAME"]?></div>
+            <?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
+                <div class="article-item__content"><?=$arItem["PREVIEW_TEXT"]?></div>
+            <?endif;?>
+        </div>
+    </a>
 <?endforeach;?>
-</div>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
 </div>
