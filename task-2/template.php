83f9990c641f089402e3d80fbaf5df2cac8527f8
Degurechaff97 <lunaplaysdota@gmail.com>
2024-07-29 07:43:04 +0300

    Added folders

---
+++ task-2/template.php
@@ -0,0 +1,52 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$this->addExternalCss($templateFolder."/css/common.css");
+?>
+
+<div class="contact-form-container">
+    <div class="contact-form">
+        <h2 class="contact-form__title">Связаться</h2>
+        <p class="contact-form__description">Наши сотрудники помогут выполнить подбор услуги и расчет цены с учетом ваших требований</p>
+
+        <?if ($arResult["isFormErrors"] == "Y"):?>
+            <div class="error-message"><?=$arResult["FORM_ERRORS_TEXT"];?></div>
+        <?endif;?>
+
+        <?=$arResult["FORM_NOTE"]?>
+
+        <?if ($arResult["isFormNote"] != "Y"):?>
+            <?=$arResult["FORM_HEADER"]?>
+            <div class="contact-form__inputs">
+                <div class="form-group">
+                    <label class="form-label" for="form_text_1">Ваше имя*</label>
+                    <input type="text" id="form_text_1" name="form_text_1" value="<?=$arResult["QUESTIONS"]["form_text_1"]["VALUE"]?>" required>
+                </div>
+                <div class="form-group">
+                    <label class="form-label" for="form_text_2">Компания/Должность*</label>
+                    <input type="text" id="form_text_2" name="form_text_2" value="<?=$arResult["QUESTIONS"]["form_text_2"]["VALUE"]?>" required>
+                </div>
+                <div class="form-group">
+                    <label class="form-label" for="form_email_3">Email*</label>
+                    <input type="email" id="form_email_3" name="form_email_3" value="<?=$arResult["QUESTIONS"]["form_email_3"]["VALUE"]?>" required>
+                </div>
+                <div class="form-group">
+                    <label class="form-label" for="form_text_4">Номер телефона*</label>
+                    <input type="tel" id="form_text_4" name="form_text_4" value="<?=$arResult["QUESTIONS"]["form_text_4"]["VALUE"]?>" required>
+                </div>
+                <div class="form-group full-width">
+                    <label class="form-label" for="form_textarea_5">Сообщение</label>
+                    <textarea id="form_textarea_5" name="form_textarea_5"><?=$arResult["QUESTIONS"]["form_textarea_5"]["VALUE"]?></textarea>
+                </div>
+            </div>
+
+            <div class="form-footer">
+                <p class="form-agreement">
+                    Нажимая «Отправить», Вы подтверждаете, что ознакомлены, полностью согласны и принимаете условия «Согласия на обработку персональных данных».
+                </p>
+                <input class="submit-btn" type="submit" name="web_form_submit" value="Оставить заявку" />
+            </div>
+
+            <?=$arResult["FORM_FOOTER"]?>
+        <?endif;?>
+    </div>
+</div>
