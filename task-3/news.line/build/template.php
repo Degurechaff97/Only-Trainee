42c8ddc220ad8146a589e62d291ce10c5f206e9b
Degurechaff97 <lunaplaysdota@gmail.com>
2024-08-01 10:36:27 +0300

    task-3

---
+++ task-3/news.line/build/template.php
@@ -0,0 +1,6 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+<div class="news-line">
+    <span class="news-date-time">15 авг 2019&nbsp;&nbsp;</span>
+    <a href="<?=$arResult["ITEMS"][0]["DETAIL_PAGE_URL"]?>">ЭР-Телеком объявляет финансовые результаты за первое полугодие 2019 года</a>
+</div>
